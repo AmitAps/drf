dff91f2c75e4b60123d96779cdbc9f1c418baa9d
Aps <aps@localhost.localdomain>
2020-11-28 03:26:38 -0500

    api using pure django

--- django_api/onlinestore/products/views.py
+++ django_api/onlinestore/products/views.py
@@ -1,11 +1,43 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+from django.http import JsonResponse
 from .models import Product, Manufacturer
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'products/product_detail.html'
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'products/product_list.html'
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = 'products/product_detail.html'
+#
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = 'products/product_list.html'
+
+def product_list(request):
+    products = Product.objects.all() #[:30] slice to show 30 product
+    data = {'products': list(products.values('pk','name'))}
+    response = JsonResponse(data)
+    return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {"products":{
+                'name': product.name,
+                'manufacturer': product.manufacturer.name,
+                'description': product.description,
+                'photo': product.photo.url,
+                'price': product.price,
+                'shipping_cost': product.shipping_cost,
+                'quantity': product.quantity,
+
+                }}
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            'error':{
+                'code': 404,
+                'message': 'Product Not Found!'
+            }},
+            status=404
+            )
+    return response
